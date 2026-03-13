902f568dd704a65dc6d01a900eebf0d77151b80e
CodeCrafter-c <anshsharma141341@gmail.com>
2026-03-13 14:43:38 +0530

    updated to store title of chat

--- Langraph/bot/bot_backend.py
+++ Langraph/bot/bot_backend.py
@@ -4,11 +4,51 @@ from langchain_core.messages import BaseMessage,HumanMessage
 from langchain_ollama import ChatOllama
 from langgraph.graph.message import add_messages 
 from langgraph.checkpoint.memory import InMemorySaver
+from pydantic import BaseModel, Field
+
 
 llm=ChatOllama(model="qwen2.5:7b")
+title_model=ChatOllama(model= "qwen2.5:7b")
+
+class Title(BaseModel):
+    title:str =Field(description="title of the chat" ,max_length=50)
+    
+structured_title_model=title_model.with_structured_output(Title)
+
 
 class ChatState(TypedDict):
     messages:Annotated[list[BaseMessage],add_messages]
+    title:str|None
+
+
+def title_generator(state: ChatState):
+
+    if state.get("title"):  
+        return {}   # already exists
+
+    first_message = state["messages"][0].content
+
+    prompt = f"""
+    Generate a short title (max 6 words) for this conversation.
+
+    Example 1
+    User message:How do I learn LangGraph step by step?
+    Generated title:Learning LangGraph Step by Step
+
+    Example 2
+    User message:Why is my Python recursion function causing a stack overflow error?
+    Generated title:Fixing Python Recursion Stack Overflow
+
+
+    Message:
+    {first_message}
+    """
+
+    res = structured_title_model.invoke(prompt)
+
+    return {"title": res.title}
+
+
     
 def chat_node(state:ChatState)->ChatState:
     messages=state["messages"]
@@ -20,11 +60,13 @@ check_pointer=InMemorySaver()
 graph=StateGraph(ChatState)
 
 #Node
+graph.add_node("title_generator", title_generator)
 graph.add_node("chat_node",chat_node)
 
 
 #edges
-graph.add_edge(START,"chat_node")
+graph.add_edge(START,"title_generator")
+graph.add_edge("title_generator","chat_node")
 graph.add_edge("chat_node",END)
 
 
